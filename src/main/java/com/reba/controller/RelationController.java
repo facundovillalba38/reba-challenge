a52f511eb5af7b880767e4f021029152decdbef9
facundo.villalba <facundo.villalba@innopulse.es>
2022-09-25 17:59:39 -0300

    Relation service creado y endpoint que devuelve relacion entre personas

--- src/main/java/com/reba/controller/RelationController.java
+++ src/main/java/com/reba/controller/RelationController.java
@@ -1,19 +1,17 @@
 package com.reba.controller;
 
+import com.reba.entity.RelationEnum;
 import com.reba.entity.dto.PersonRelationDto;
 import com.reba.service.RelationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/relacion")
+@RequestMapping("/relaciones")
 public class RelationController {
 
     private RelationService relationService;
@@ -27,5 +25,10 @@ public class RelationController {
     private ResponseEntity relatePerson(@Valid @RequestBody PersonRelationDto personRelationDto){
         relationService.relatePerson(personRelationDto);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/{dni1}/{dni2}")
+    private ResponseEntity<String> returnRelation(@PathVariable("dni1") String dni1, @PathVariable("dni2") String dni2){
+        return new ResponseEntity(relationService.returnRelation(dni1, dni2), HttpStatus.OK);
     }
 }
--- src/main/java/com/reba/service/RelationService.java
+++ src/main/java/com/reba/service/RelationService.java
@@ -1,5 +1,6 @@
 package com.reba.service;
 
+import com.reba.entity.RelationEnum;
 import com.reba.entity.db.Person;
 import com.reba.entity.db.Relation;
 import com.reba.entity.dto.PersonRelationDto;
@@ -30,18 +31,19 @@ public class RelationService {
         Relation relation = relationRepository.findByRelation(personRelationDto.getRelation());
         if(p1 == null || p2 == null){
             throw new ApiCustomException("Ambos o algun DNI ingresado es incorrecto.");
-        }else{
-            // se chequea la relacion previa de ambos lados, tanto p1 con respecto a p2 y viceversa.
-            // Si no hay relacion se procede a crearla
-            if(!checkPreviousRelation(p1, p2) && !checkPreviousRelation(p2,p1)){
-                Map<Person, Relation> personRelationMap = new HashMap<>();
-                personRelationMap.put(p2, relation);
-                p1.setRelation(personRelationMap);
-                personRepository.save(p1);
-            }else{
-                throw new ApiCustomException("Las dos personas ingresadas ya tenian una relacion previa.");
-            }
         }
+
+        // se chequea la relacion previa de ambos lados, tanto p1 con respecto a p2 y viceversa.
+        // Si no hay relacion se procede a crearla
+        if(!checkPreviousRelation(p1, p2) && !checkPreviousRelation(p2,p1)){
+            Map<Person, Relation> personRelationMap = new HashMap<>();
+            personRelationMap.put(p2, relation);
+            p1.setRelation(personRelationMap);
+            personRepository.save(p1);
+        }else{
+            throw new ApiCustomException("Las dos personas ingresadas ya tenian una relacion previa.");
+        }
+
     }
 
     private Boolean checkPreviousRelation(Person p1, Person p2){
@@ -50,5 +52,23 @@ public class RelationService {
             return true;
         }else
             return false;
+    }
+
+    public String returnRelation(String dni1, String dni2) {
+        Person p1 = personRepository.findPersonByDni(dni1);
+        Person p2 = personRepository.findPersonByDni(dni2);
+        if(p1 == null || p2 == null){
+            throw new ApiCustomException("Ambos o algun DNI ingresado es incorrecto.");
+        }
+        String relation="";
+        if(checkPreviousRelation(p1,p2)){
+            relation = p1.getRelation().get(p2).getRelation().name();
+        }
+        if(checkPreviousRelation(p2,p1)){
+            relation = p2.getRelation().get(p1).getRelation().name();
+        }
+
+        return relation;
+
     }
 }
